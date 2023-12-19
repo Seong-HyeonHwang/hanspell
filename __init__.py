508bffd7410fe532ff55e250a37c06b1efa4f809
seokhyeon-Park <dfbgq346q@gmail.com>
2023-12-19 17:58:42 +0900

    *

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
+from .constants import *
+from .response import *
+from .spell_checker import *
+
 __version__ = '1.1'
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='hanspell',
+  version='1.1',
+  description='tuned by seokbong (for testing)',
+  url='https://github.com/Seokhyeon-Park/hanspell.git',
+  author='Seokbong',
+  author_email='dfbgq346q@gmail.com',
+  license='MIT',
+  packages=find_packages(),
+  install_requires=['requests']
+)
