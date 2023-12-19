bfef6e295f8db3fdfad3fd9ef65d24177b68e120
seokhyeon-Park <dfbgq346q@gmail.com>
2023-12-19 18:48:36 +0900

    *

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hanspell',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    author='Seokbong',
+    author_email='dfbgq346q@gmail.com',
+    description='editing hanspell package (for testing)',
+    url='https://github.com/Seokhyeon-Park/hanspell.git',
+)
